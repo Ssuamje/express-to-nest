06a8882149813beb3de25c6176c9db13396d072b
Ssuamje <side2850@naver.com>
2024-01-26 20:36:35 +0900

    feat : e2e test - 컨트롤러를 꽂아보자

--- test/e2e/app.start.e2e.test.ts
+++ test/e2e/app.start.e2e.test.ts
@@ -1,5 +1,32 @@
-describe("app.start", () => {
-  it("should start app", () => {
-    expect(true).toBeTruthy();
+/**
+ * https://docs.nestjs.com/fundamentals/testing
+ */
+
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../../src/app.module";
+import { UserController } from "../../src/user/user.controller";
+import { INestApplication } from "@nestjs/common";
+
+describe("E2E 테스트를 시작한다 - UserController", () => {
+  let app: INestApplication;
+  let userController: UserController;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(UserController)
+      .useValue(userController)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  describe("아마도 Describe는 Nesting이 될테지", () => {
+    it("it은 Describe 안에만 있으면 된다.", () => {
+      // it을 given으로 바꾸고, when, then만 걸어서 표기하면 볼만하겠지.
+      expect(true).toBeTruthy();
+    });
   });
 });
